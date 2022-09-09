86d4214fe5986db79da7a1d166a60aa907b61b5c
Aziz-Mattar <aziz.balaha@hotmail.com>
2022-09-09 22:07:02 +0300

    Adding Theme Features

--- functions.php
+++ functions.php
@@ -13,22 +13,18 @@ if (!function_exists('wpc_load_assets')) {
         wp_enqueue_style('wpc_colors', $base . 'assets/css/colors.css', [], null);
 
         wp_enqueue_script('wpc_jquery', $base . 'assets/js/jquery.min.js', [], null, true);
-        // wp_enqueue_script('jquery');
         wp_enqueue_script('wpc_tether', $base . 'assets/js/tether.min.js', [], null, true);
         wp_enqueue_script('wpc_bootstrap', $base . 'assets/js/bootstrap.min.js', [], null, true);
         wp_enqueue_script('wpc_custom', $base . 'assets/js/custom.js', [], null, true);
 
     }
     add_action('wp_enqueue_scripts', 'wpc_load_assets');
-} else {
-    // notify the admin
 }
 
-// if (!function_exists('wpc_redefine_assets')) {
-//     function wpc_redefine_assets()
-//     {
-//         wp_deregister_script('jquery');
-//         wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.min.js', [], '3.5.1');
-//     }
-//     add_action('wp_enqueue_scripts', 'wpc_redefine_assets');
-// }
+if (!function_exists('wpc_setup')) {
+    function wpc_setup()
+    {
+        add_theme_support('post-thumbnails');
+    }
+    add_action('after_setup_theme', 'wpc_setup');
+}
