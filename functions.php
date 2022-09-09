9f286824110238d64b22a862b2c71700c6e46c3b
Aziz-Mattar <aziz.balaha@hotmail.com>
2022-09-09 22:05:43 +0300

    Including Assets (2)

--- functions.php
+++ functions.php
@@ -1,20 +1,34 @@
 <?php 
 
-function load_assets()
-{
+if (!function_exists('wpc_load_assets')) {
+    function wpc_load_assets()
+    {
 
-    $base = get_template_directory_uri() . '/';
+        $base = get_template_directory_uri() . '/';
 
-    wp_enqueue_style('bootstrap', $base . 'assets/css/bootstrap.css', [], false);
-    wp_enqueue_style('fontsawesome', $base . 'assets/css/font-awesome.min.css', ['bootstrap'], false);
-    wp_enqueue_style('template-style', $base . 'assets/style.css', [], false);
-    wp_enqueue_style('template-responsive', $base . 'assets/css/responsive.css', [], false);
-    wp_enqueue_style('template-colors', $base . 'assets/css/colors.css', [], false);
+        wp_enqueue_style('wpc_bootstrap', $base . 'assets/css/bootstrap.css', [], null);
+        wp_enqueue_style('wpc_fontawesome', $base . 'assets/css/font-awesome.min.css', ['wpc_bootstrap'], null);
+        wp_enqueue_style('wpc_style', $base . 'assets/style.css', [], null);
+        wp_enqueue_style('wpc_responsive', $base . 'assets/css/responsive.css', [], null);
+        wp_enqueue_style('wpc_colors', $base . 'assets/css/colors.css', [], null);
 
-    wp_enqueue_script('jq-script', $base . 'assets/js/jquery.min.js', [], false, true);
-    wp_enqueue_script('tether-script', $base . 'assets/js/tether.min.js', [], false, true);
-    wp_enqueue_script('bootstrap-script', $base . 'assets/js/bootstrap.min.js', [], false, true);
-    wp_enqueue_script('custom-script', $base . 'assets/js/custom.js', [], false, true);
+        wp_enqueue_script('wpc_jquery', $base . 'assets/js/jquery.min.js', [], null, true);
+        // wp_enqueue_script('jquery');
+        wp_enqueue_script('wpc_tether', $base . 'assets/js/tether.min.js', [], null, true);
+        wp_enqueue_script('wpc_bootstrap', $base . 'assets/js/bootstrap.min.js', [], null, true);
+        wp_enqueue_script('wpc_custom', $base . 'assets/js/custom.js', [], null, true);
 
+    }
+    add_action('wp_enqueue_scripts', 'wpc_load_assets');
+} else {
+    // notify the admin
 }
-add_action('wp_enqueue_scripts', 'load_assets');
+
+// if (!function_exists('wpc_redefine_assets')) {
+//     function wpc_redefine_assets()
+//     {
+//         wp_deregister_script('jquery');
+//         wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.min.js', [], '3.5.1');
+//     }
+//     add_action('wp_enqueue_scripts', 'wpc_redefine_assets');
+// }
