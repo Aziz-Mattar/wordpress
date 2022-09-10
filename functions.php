90e56eb0c67e68639830d331ce66e4744be4bf8b
Aziz-Mattar <aziz.balaha@hotmail.com>
2022-09-10 11:35:00 +0300

    3.Single Post Title and Content

--- functions.php
+++ functions.php
@@ -3,24 +3,19 @@
 if (!function_exists('wpc_load_assets')) {
     function wpc_load_assets()
     {
-
         $base = get_template_directory_uri() . '/';
-
         wp_enqueue_style('wpc_bootstrap', $base . 'assets/css/bootstrap.css', [], null);
         wp_enqueue_style('wpc_fontawesome', $base . 'assets/css/font-awesome.min.css', ['wpc_bootstrap'], null);
         wp_enqueue_style('wpc_style', $base . 'assets/style.css', [], null);
         wp_enqueue_style('wpc_responsive', $base . 'assets/css/responsive.css', [], null);
         wp_enqueue_style('wpc_colors', $base . 'assets/css/colors.css', [], null);
-
         wp_enqueue_script('wpc_jquery', $base . 'assets/js/jquery.min.js', [], null, true);
         wp_enqueue_script('wpc_tether', $base . 'assets/js/tether.min.js', [], null, true);
         wp_enqueue_script('wpc_bootstrap', $base . 'assets/js/bootstrap.min.js', [], null, true);
         wp_enqueue_script('wpc_custom', $base . 'assets/js/custom.js', [], null, true);
-
     }
     add_action('wp_enqueue_scripts', 'wpc_load_assets');
 }
-
 if (!function_exists('wpc_setup')) {
     function wpc_setup()
     {
@@ -28,3 +23,9 @@ if (!function_exists('wpc_setup')) {
     }
     add_action('after_setup_theme', 'wpc_setup');
 }
+
+function wpc_edit_content($content)
+{
+    return $content . '<p>Edited</p>';
+}
+add_filter('the_content', 'wpc_edit_content');
