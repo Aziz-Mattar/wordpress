7ec5d10df760025bc3e7e73fcf34a2b6c434b8d5
Aziz-Mattar <aziz.balaha@hotmail.com>
2022-09-17 09:06:54 -0700

    5.6. Sample Widget Popular Posts (2)

--- inc/widgets/popular_posts.php
+++ inc/widgets/popular_posts.php
@@ -8,12 +8,14 @@ class Wpc_Popular_Posts extends WP_Widget
 
     function widget($args, $instance) {
         echo $args['before_widget'];
-        echo $args['before_title'] . 'Popular Posts' . $args['after_title'];
+        if (isset($instance['title'])) {
+            echo $args['before_title'] . $instance['title'] . $args['after_title'];
+        }
         $popular_posts = get_posts([
             'orderby' => 'meta_value_num',
             'meta_key' => 'wpc_post_views',
             'order' => 'desc',
-            'numberposts' => 4
+            'numberposts' => (isset($instance['posts_count']) && is_numeric($instance['posts_count']) && $instance['posts_count'] > 0) ? $instance['posts_count'] : 4,
         ]);
         if (count($popular_posts)) {
             echo '<div class="blog-list-widget"><div class="list-group">';
@@ -24,7 +26,13 @@ class Wpc_Popular_Posts extends WP_Widget
                         <?php echo get_the_post_thumbnail($post, 'thumbnail', ['class' => 'img-fluid float-left']) ?>
                         <h5 class="mb-1"><?php echo $post->post_title; ?></h5>
                         <small>
-                        <i class="fa fa-eye"></i><?php echo ((int)(get_post_meta($post->ID, 'wpc_post_views', true))) ?>
+                        <?php
+                        if (isset($instance['alt_content']) && $instance['alt_content'] == 'post_views') {
+                            ?><i class="fa fa-eye"></i><?php echo ((int)(get_post_meta($post->ID, 'wpc_post_views', true))) ?><?php
+                        } else {
+                            echo get_the_date('d M, Y', $post);
+                        }
+                        ?>
                         </small>
                     </div>
                 </a>
@@ -33,6 +41,41 @@ class Wpc_Popular_Posts extends WP_Widget
             echo '</div></div>';
         }
         echo $args['after_widget'];
+    }
+
+    function form($instance) {
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        } else {
+            $title = 'Popular Posts';
+        }
+        if (isset($instance['posts_count'])) {
+            $posts_count = $instance['posts_count'];
+        } else {
+            $posts_count = 4;
+        }
+        if (isset($instance['alt_content'])) {
+            $alt_content = $instance['alt_content'];
+        } else {
+            $alt_content = 'post_date';
+        }
+        ?>
+        <p>
+            <label for="">Widget Title</label>
+            <input type="text" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>">
+        </p>
+        <p>
+            <label for="">Posts Count</label>
+            <input type="number" name="<?php echo $this->get_field_name('posts_count'); ?>" value="<?php echo $posts_count; ?>" min="1" id="">
+        </p>
+        <p>
+            <label for="">Alt Content</label>
+            <select name="<?php echo $this->get_field_name('alt_content') ?>" id="">
+                <option value="post_date" <?php echo ($alt_content == 'post_date') ? 'selected' : ''; ?>>Post date</option>
+                <option value="post_views" <?php echo ($alt_content == 'post_views') ? 'selected' : ''; ?>>Post views</option>
+            </select>
+        </p>
+        <?php
     }
 }
 
